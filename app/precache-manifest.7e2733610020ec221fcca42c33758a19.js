c1d260a8e45a1bcceb68d6702647bd6fd3fc76bf
tomnijhof <tom.nijhof@cytosmart.com>
2021-12-07 22:53:14 +0100

    add first app
    Signed-off-by: tomnijhof <tom.nijhof@cytosmart.com>

---
+++ app/precache-manifest.7e2733610020ec221fcca42c33758a19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bec3745e75bd6d48706",
+    "url": "/css/app.ab0a4183.css"
+  },
+  {
+    "revision": "c3a6768b9dd7b6bc7a0c2cd4300911e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bec3745e75bd6d48706",
+    "url": "/js/app.e419613a.js"
+  },
+  {
+    "revision": "5257701fba503407ec1d",
+    "url": "/js/chunk-vendors.aa27a087.js"
+  },
+  {
+    "revision": "e75cbd418403c1b020e5d3009e4c48d6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
